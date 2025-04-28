fb43f644b362675cac277e349cac6053450bdb73
tsamcservice <tsamcservice@gmail.com>
2025-04-28 11:24:05 +0800

    test: 更新主頁面內容以測試部署

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,14 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  /* config options here */
+  reactStrictMode: true,
+  swcMinify: true,
+  typescript: {
+    ignoreBuildErrors: false,
+  },
+  eslint: {
+    ignoreDuringBuilds: false,
+  },
 };
 
 export default nextConfig;
